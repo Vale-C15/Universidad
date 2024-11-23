887139de7c8d0a05a4aca673dece03308b872352
Valeria Cardona <valeriacardona1116177@correo.itm.edu.co>
2024-11-23 14:25:24 -0500

    Creación libreria entidades

---
+++ Universidad/lib_entidades/Modelos/Estados.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace lib_entidades.Modelos
+{
+    public class Estados
+    {
+        [Key] public int Id { get; set; } // 1. Activo     2. Inactivo
+        public string? Nombre { get; set; }
+
+    }
+}
