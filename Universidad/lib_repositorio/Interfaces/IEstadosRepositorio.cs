7bd7f0f2e0c5d3ab60aa40c059cea2cb3b7e6d01
riosj <jr185888@gmail.com>
2024-11-23 15:20:19 -0500

    Creacion de las interfaces del repositorio

---
+++ Universidad/lib_repositorio/Interfaces/IEstadosRepositorio.cs
@@ -0,0 +1,20 @@
+﻿using lib_entidades.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lib_repositorios.Interfaces
+{
+    public interface IEstadosRepositorio
+    {
+        void Configurar(string string_conexion);
+        List<Estados> Listar();
+        List<Estados> Buscar(Expression<Func<Estados, bool>> condiciones);
+        Estados Guardar(Estados entidad);
+        Estados Modificar(Estados entidad);
+        Estados Borrar(Estados entidad);
+    }
+}
