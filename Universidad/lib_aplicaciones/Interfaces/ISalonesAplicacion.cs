a74ffdc610fcdd9e2f780b8ca6e3ed15beca0c70
Valeria Cardona <valeriacardona1116177@correo.itm.edu.co>
2024-11-24 14:32:54 -0500

    Creacion Interfaces Lib_aplicaciones

---
+++ Universidad/lib_aplicaciones/Interfaces/ISalonesAplicacion.cs
@@ -0,0 +1,20 @@
+﻿using lib_entidades.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lib_aplicaciones.Interfaces
+{
+    public interface ISalonesAplicacion
+    {
+        void Configurar(string string_conexion);
+        List<Salones> Listar();
+        List<Salones> Buscar(Salones entidad, string tipo);
+        Salones Guardar(Salones entidad);
+        Salones Modificar(Salones entidad);
+        Salones Borrar(Salones entidad);
+    }
+}
