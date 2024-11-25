ba15e12fedd21a5047923a1987b48f314d30febf
riosj <jr185888@gmail.com>
2024-11-25 00:04:49 -0500

    Creacion de las implementaciones en la lib presentaciones

---
+++ Universidad/lib_presentaciones/Implementaciones/MateriasPresentacion.cs
@@ -0,0 +1,96 @@
+﻿using lib_comunicaciones.Interfaces;
+using lib_entidades.Modelos;
+using lib_presentaciones.Interfaces;
+using lib_utilidades;
+
+namespace lib_presentaciones.Implementaciones
+{
+    public class MateriasPresentacion : IMateriasPresentacion
+    {
+        private IMateriasComunicacion? iComunicacion = null;
+        public MateriasPresentacion(IMateriasComunicacion iComunicacion)
+        {
+            this.iComunicacion = iComunicacion;
+        }
+        public async Task<List<Materias>> Listar()
+        {
+            var lista = new List<Materias>();
+            var datos = new Dictionary<string, object>();
+            var respuesta = await iComunicacion!.Listar(datos);
+            if (respuesta.ContainsKey("Error"))
+            {
+                throw new Exception(respuesta["Error"].ToString()!);
+            }
+            lista = JsonConversor.ConvertirAObjeto<List<Materias>>(
+                JsonConversor.ConvertirAString(respuesta["Entidades"]));
+            return lista;
+        }
+        public async Task<List<Materias>> Buscar(Materias entidad, string tipo)
+        {
+            var lista = new List<Materias>();
+            var datos = new Dictionary<string, object>();
+            datos["Entidad"] = entidad;
+            datos["Tipo"] = tipo;
+            var respuesta = await iComunicacion!.Buscar(datos);
+            if (respuesta.ContainsKey("Error"))
+            {
+                throw new Exception(respuesta["Error"].ToString()!);
+            }
+            lista = JsonConversor.ConvertirAObjeto<List<Materias>>(
+                JsonConversor.ConvertirAString(respuesta["Entidades"]));
+            return lista;
+        }
+        public async Task<Materias> Guardar(Materias entidad)
+        {
+            if (entidad.Id != 0
+                || !entidad.Validar())
+            {
+                throw new Exception("lbFaltaInformacion");
+            }
+            var datos = new Dictionary<string, object>();
+            datos["Entidad"] = entidad;
+            var respuesta = await iComunicacion!.Guardar(datos);
+            if (respuesta.ContainsKey("Error"))
+            {
+                throw new Exception(respuesta["Error"].ToString()!);
+            }
+            entidad = JsonConversor.ConvertirAObjeto<Materias>(
+                JsonConversor.ConvertirAString(respuesta["Entidad"]));
+            return entidad;
+        }
+        public async Task<Materias> Modificar(Materias entidad)
+        {
+            if (entidad.Id == 0 || !entidad.Validar())
+            {
+                throw new Exception("lbFaltaInformacion");
+            }
+            var datos = new Dictionary<string, object>();
+            datos["Entidad"] = entidad;
+            var respuesta = await iComunicacion!.Modificar(datos);
+            if (respuesta.ContainsKey("Error"))
+            {
+                throw new Exception(respuesta["Error"].ToString()!);
+            }
+            entidad = JsonConversor.ConvertirAObjeto<Materias>(
+                JsonConversor.ConvertirAString(respuesta["Entidad"]));
+            return entidad;
+        }
+        public async Task<Materias> Borrar(Materias entidad)
+        {
+            if (entidad.Id == 0 || !entidad.Validar())
+            {
+                throw new Exception("lbFaltaInformacion");
+            }
+            var datos = new Dictionary<string, object>();
+            datos["Entidad"] = entidad;
+            var respuesta = await iComunicacion!.Borrar(datos);
+            if (respuesta.ContainsKey("Error"))
+            {
+                throw new Exception(respuesta["Error"].ToString()!);
+            }
+            entidad = JsonConversor.ConvertirAObjeto<Materias>(
+                JsonConversor.ConvertirAString(respuesta["Entidad"]));
+            return entidad;
+        }
+    }
+}
